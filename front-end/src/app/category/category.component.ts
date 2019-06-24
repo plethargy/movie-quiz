572f9cc2e0425e4546121aac636454f0e0f6f837
MKHULULI MTSHEMLA <201402344@student.uj.ac.za>
2019-06-24 14:59:27 +0200

    category ui and routing

--- front-end/src/app/category/category.component.ts
+++ front-end/src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  goToCategory(category: number) {
+    this.router.navigate(['/questions',category]);
+  }
+  
+  
 }
