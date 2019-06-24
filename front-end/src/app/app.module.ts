9eb765c267dc3141bfcac035f5a78d30f480931a
mkay47 <201402344@student.uj.ac.za>
2019-06-24 15:07:19 +0200

    Revert "Feature category"

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CategoryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
