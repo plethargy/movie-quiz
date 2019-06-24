b68c7a3d3a20fbc101df8bdcdb632022c0750415
MKHULULI MTSHEMLA <201402344@student.uj.ac.za>
2019-06-24 16:53:31 +0200

    added leaderboard

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 const routes: Routes = [
-  { path: 'category', component: CategoryComponent }
+  { path: 'category', component: CategoryComponent },
+  { path: 'leaderboard', component: LeaderboardComponent }
 ];
 
 @NgModule({
