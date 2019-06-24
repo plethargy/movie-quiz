0835943af8265a7b9b35e933c22a1965b287a61d
Bevan <bevan.venter@yahoo.com>
2019-06-24 16:21:22 +0200

    Added Bootstrap
    Added Bootstrap

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { SummaryComponent } from './summary/summary.component';
 
+//************************************************************************************
+// BOOTSTRAP IMPORT
+//************************************************************************************
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +19,8 @@ import { SummaryComponent } from './summary/summary.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
