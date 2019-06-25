84ab31d96eed88751067d9e6184dceb533a5d8dc
Bevan <bevan.venter@yahoo.com>
2019-06-25 13:21:32 +0200

    Added timer

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { QuestionsComponent } from './questions/questions.component';
 import { SummaryComponent } from './summary/summary.component';
 import { CategoryComponent } from './category/category.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
-import {UserService} from './services/user.service';
+import { UserService } from './services/user.service';
 
 //************************************************************************************
 // CUSTOM IMPORTS
