e2e9b9ec4fb595ffd73170b63ce4aa2050db8c43
Bevan <bevan.venter@yahoo.com>
2019-06-25 13:38:02 +0200

    Routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/category', pathMatch: 'full' },
   { path: 'category', component: CategoryComponent },
-  { path: 'leaderboard', component: LeaderboardComponent }
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'question/:id', component: QuestionsComponent },
+  { path: '**', redirectTo: ''}
+
 ];
 
 @NgModule({
